216d8c46ad628a223f1ccc472eb8e0402351afc1
MundumCustos <russiannik11@gmail.com>
2021-08-30 20:31:13 +0300

    first commit

---
+++ works/site-Auto/js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+    $('.carousel__wrapper').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        responsive: [
+            {
+              breakpoint: 841,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 601,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+        ]
+    });
+});
